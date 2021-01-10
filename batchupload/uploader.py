9f85d9ac10fe5ee34d91e0a45979c1a026459ac0
lokal-profil <lokal.profil@gmail.com>
2021-01-11 00:26:12 +0100

    add svg and png as recognized file extensions

--- batchupload/uploader.py
+++ batchupload/uploader.py
@@ -8,7 +8,7 @@ from batchupload.make_info import make_info_page
 import os
 import pywikibot
 
-FILE_EXTS = ('.tif', '.jpg', '.tiff', '.jpeg', '.wav')
+FILE_EXTS = ('.tif', '.jpg', '.tiff', '.jpeg', '.wav', '.svg', '.png')
 URL_PROTOCOLS = ('http', 'https')  # @todo: extend with supported protocols
 
 
